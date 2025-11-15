beb33cceb20d6c96f06a128950be592725c5a174
shamandeer <aqshabgs13@gmail.com>
2025-11-15 22:51:06 +0700

    feat: rebuild game with placeholder assets (movement, audio, platform)
    adds animation sprites, collisions, basic movement script, audio, platformer placeholder, yeah

---
+++ cat-game/scripts/movement.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+@onready var player_anim = $AnimatedSprite2D
+@onready var sfx_jump = $sfx_jump
+@onready var sfx_bonk = $sfx_bonk
+
+const MOVE_SPEED = 100
+const JUMP_FORCE = 250
+const GRAVITY = 500
+
+func _physics_process(delta: float) -> void:
+	#Gravity
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	
+	# Left-Right Movement
+	var direction := Input.get_axis('Left', 'Right')
+	if direction:
+		velocity = Vector2(direction * MOVE_SPEED, velocity.y)
+		player_anim.play('walk')
+	else:
+		velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
+		player_anim.play('idle')
+	
+	# Jump
+	if Input.is_action_just_pressed('Up') and is_on_floor():
+		velocity.y -= JUMP_FORCE
+		sfx_jump.play() 
+	
+	# Direction
+	if direction == 1:
+		player_anim.flip_h = false
+	elif direction == -1:
+		player_anim.flip_h = true
+	
+	
+	move_and_slide()
+	if is_on_ceiling():
+			sfx_jump.stop()
+			sfx_bonk.play()
+			
