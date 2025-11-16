47aed12e7862d4a3a31c93c5b108e816c69b92d4
Fathar Atandra Denaya <fathar.atandra@gmail.com>
2025-11-16 21:28:34 +0700

    Fix function signature by adding a colon

--- cat-game/scenes/placeholder_ground.gd
+++ cat-game/scenes/placeholder_ground.gd
@@ -3,4 +3,4 @@ extends StaticBody2D
 @onready var camera_enter = $Camera2D
 @onready var player_pos = $Player
 
-func _on_area_2d_body_entered(body: Node2D) -> void
+func _on_area_2d_body_entered(body: Node2D) -> void:
