c828bb2497f662624bb82c338d5b591504584109
ryualdebaran <ryu.aldebaran@gmail.com>
2025-11-17 23:04:36 +0700

    first build

--- cat-game/scenes/camera.gd
+++ cat-game/scenes/camera.gd
@@ -1,12 +1,8 @@
 extends Camera2D
 
-const CAMERA_SPEED = 120
+const CAMERA_SPEED = 70
 
 @onready var camera_triggered = false
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
