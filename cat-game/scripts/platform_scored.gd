1a29e1b006f925b9e85f5f16c74fe1109de2a9d1
ryualdebaran <ryu.aldebaran@gmail.com>
2025-11-21 09:11:13 +0700

    fix: memasukkan player ke dalam global group 'player'

--- cat-game/scripts/platform_scored.gd
+++ cat-game/scripts/platform_scored.gd
@@ -3,7 +3,7 @@ var is_touched := false
 var value := 1
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if (is_touched == false) and (body.is_class('player')):
+	if (is_touched == false) and (body.is_in_group('player')):
 		print('+1 platform score')
 		GameController.platform_scored(value)
 		is_touched = true
