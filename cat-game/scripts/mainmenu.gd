d3a684c8027ea7586c3d49d60d11fec9ccbd104b
ryualdebaran <ryu.aldebaran@gmail.com>
2025-11-23 16:47:12 +0700

    feat: added main menunya mas nabil

---
+++ cat-game/scripts/mainmenu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var click = $click
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+#
+	
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_playbutton_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
