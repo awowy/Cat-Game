ca0247f30a7bf9aaad52ccd881f61df8fa7fbf63
ryualdebaran <ryu.aldebaran@gmail.com>
2025-11-27 23:15:57 +0700

    fix: ngubah musik pas dia mati

--- cat-game/scripts/killzone.gd
+++ cat-game/scripts/killzone.gd
@@ -7,5 +7,6 @@ func _on_body_entered(body: Node2D) -> void:
 		timer.start()
 	
 func _on_timer_timeout():
+	MusicPlayer.play_music(load("res://assets/audios/Strings.wav"))
 	EventController.emit_signal("game_over")
 	get_tree().change_scene_to_file("res://scenes/gameover.tscn")
