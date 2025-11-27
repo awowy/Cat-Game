428f60d919b5573f2fbeab3c83216a9c65fa8c72
ryualdebaran <ryu.aldebaran@gmail.com>
2025-11-27 23:02:10 +0700

    feat: can play bgm now

--- cat-game/scripts/mainmenu.gd
+++ cat-game/scripts/mainmenu.gd
@@ -2,7 +2,7 @@ extends Control
 @onready var click = $click
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	MusicPlayer.play_music(load("res://assets/audios/mp3musik.mp3"))
 
 func _on_quit_pressed():
 	click.play()
@@ -17,6 +17,7 @@ func _on_play_pressed() -> void:
 	click.play()
 	await click.finished
 	if GameController.has_played != true:
+		MusicPlayer.play_music(load("res://assets/audios/Strings.wav"))
 		get_tree().change_scene_to_file("res://scenes/cutscene.tscn")
 	elif GameController.has_played == true:
 		get_tree().change_scene_to_file("res://scenes/main.tscn")
