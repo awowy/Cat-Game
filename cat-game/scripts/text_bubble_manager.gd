819a43ac7c22ff3db879ab2b9d9d6c5fbe136c88
shamandeer <aqshabgs13@gmail.com>
2025-11-27 14:06:52 +0700

    added group for early windows and insects. fixed collision toggles in tutorial

--- cat-game/scripts/text_bubble_manager.gd
+++ cat-game/scripts/text_bubble_manager.gd
@@ -5,9 +5,6 @@ var insect_msg = preload("res://scenes/insect_bubble.tscn")
 var platform_msg = preload("res://scenes/platform_bubble.tscn")
 var message_spawned = false
 @onready var player = $"../Player"
-@onready var window1 = $"../Placeholder/windowplatform/CollisionShape2D"
-@onready var window2 = $"../Placeholder/windowplatform2/CollisionShape2D"
-@onready var insect = $"../Insects/Sprite2D/Area2D/CollisionShape2D"
 
 func _ready() -> void:
 	if player != null and message_spawned == false:
@@ -18,36 +15,22 @@ func ManageMessage():
 		var move_instance = move_msg.instantiate()
 		player.add_child(move_instance)
 		move_instance.position = Vector2(25, -25)
-		DisableCollisions()
-		DisableInsect()
+		get_tree().call_group("window", "DisableCollisions")
+		get_tree().call_group("Insect", "DisableInsect")
 		
 		await move_instance.tree_exited
 		
 		var insect_instance = insect_msg.instantiate()
 		player.add_child(insect_instance)
 		insect_instance.position = Vector2(25, -25)
-		EnableInsect()
+		get_tree().call_group("Insect", "EnableInsect")
 		
 		await insect_instance.tree_exited
 		
 		var platform_instance = platform_msg.instantiate()
 		player.add_child(platform_instance)
 		platform_instance.position = Vector2(25, -25)
-		EnableCollisions()
+		get_tree().call_group("window", "EnableCollisions")
 		
 		await platform_instance.tree_exited
 		return
-		
-func DisableCollisions():
-	window1.set_deferred("disabled", true)
-	window2.set_deferred("disabled", true)
-	
-func DisableInsect():
-	insect.set_deferred("disabled", true)
-
-func EnableCollisions():
-	window1.set_deferred("disabled", false)
-	window2.set_deferred("disabled", false)
-
-func EnableInsect():
-	insect.set_deferred("disabled", false)
