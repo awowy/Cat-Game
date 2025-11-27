3433bc3019d952cb71e038331ebf4ed30e5a013f
Ryuza Nadif Aldebaran <ryu.aldebaran@gmail.com>
2025-11-27 15:03:16 +0700

    Revert "Revert "Merge branch 'final-build' into speech_bubble_branch""

--- cat-game/scripts/gameover.gd
+++ cat-game/scripts/gameover.gd
@@ -1,5 +1,5 @@
 extends Control
-
+@onready var click = $click
 
 
 var is_gameover = true
@@ -11,11 +11,21 @@ func _process(delta: float) -> void:
 	if visible and not is_gameover:
 		is_gameover = true
 
-func _on_retrybutton_pressed():
+
+
+
+
+
+func _on_retry_pressed() -> void:
+	click.play()
+	await click.finished
 	get_tree().paused = false
 	visible = false
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
-func _on_homebutton_pressed():
+
+func _on_home_pressed() -> void:
+	click.play()
+	await click.finished
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
